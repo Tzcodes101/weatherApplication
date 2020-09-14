1ebc6ae2dad98d366566c191b0bbdcdff38e73cf
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-14 10:49:15 -0400

    startover, new pseudocode

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
 $( document ).ready(function() {
 
-    //add click event to search button from "Search For City" that
+//on click event to search button
 
-        // grabs the input value (the city) and stores it in a var
+//on click event to history buttons from search history
 
-        //create an API key for the OpenWeather API
+//add to search history (makeRow function)
 
-        //create var to get value from input (city search)
-        //create var queryURL = https://openweathermap.org/api + value var + API key
+//search for weather
 
-        // get value from input
+//obtain the forecast
 
-        // call open weatherAPI with input value through an ajax request 
-
-        //display results
-
-    //if index above a certain limit, change CSS class to severe (red)
-        //else if below that limit and above another, change CSS class to moderate
-        //else, change CSS class to mild
-
-    //look up all possible forcasts and create associated icon with each forecast.
-        //if forecast = sunny, change icon to sun
-        //else if forecast = clouy, change icon to cloudy
-        //else if, etc... 
-
-
-    //add click event to search button from "Search For City" that
-
-        // grabs the input value (the city) and stores it in a var
-
-        //this input value is then added to search history and displayed to page
-
-
-    //add click event to buttons in search history so that user can click on the button and get the present and future forecast for the corresponding city (this).val() maybe...?
-        //so when I click on a history button, then I get present and future forecast
-            //can I do this by somehow setting the button content equal to the input value from the search box, then calling initial function
-            //or just create new function using same initial code but different input var, however, it grabs the search-history-button value that was clicked, instead of the value from the search-for-city input.
+//obtain UV index
 
 
 
-
-
-
-
-
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!! when load page:
+//get current history from local storage, if any
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
